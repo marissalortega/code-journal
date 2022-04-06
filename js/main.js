513c81b93f2adcb9a67ff67b891a8a6dbcae0177
Marissa Ortega <marissaljames@gmail.com>
2022-04-05 19:18:45 -0700

    Listen for 'submit' events on the journal entry form - all sub-bullets #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 /* global data */
 /* exported data */
-var photoURLInput = document.querySelector('.photo-url');
-var image = document.querySelector('img');
+var $photoURL = document.querySelector("input[name = 'photourl']");
+var $image = document.querySelector('img');
 
-photoURLInput.addEventListener('input', function (event) {
-  image.setAttribute('src', event.target.value);
+$photoURL.addEventListener('input', function (event) {
+  $image.setAttribute('src', event.target.value);
+});
+
+var journalEntryForm = document.getElementById('journal-entry-form');
+
+journalEntryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var journalEntry = {
+    title: journalEntryForm.elements.title.value,
+    photoURL: journalEntryForm.elements.photourl.value,
+    notes: journalEntryForm.elements.usernotes.value
+  };
+  journalEntry.entryId = data.nextEntryId++;
+  data.entries.unshift(journalEntry);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  document.getElementById('journal-entry-form').reset();
 });
